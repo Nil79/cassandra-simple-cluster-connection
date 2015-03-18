c3e94529704c6318a3af54bcf7b199f742543d77
Nicola Sodano <sodano.n@gmail.com>
2015-03-18 17:10:38 +0100

    Junit for Main Class

--- src/test/java/com/nicolasodano/cassandra/MainTest.java
+++ src/test/java/com/nicolasodano/cassandra/MainTest.java
@@ -1,13 +1,13 @@
-package com.nicolasodano.cassandra.simple_cluster_connection;
+package com.nicolasodano.cassandra;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for Man.
  */
-public class AppTest 
+public class MainTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public MainTest( String testName )
     {
         super( testName );
     }
@@ -25,13 +25,13 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( MainTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testMain()
     {
         assertTrue( true );
     }
